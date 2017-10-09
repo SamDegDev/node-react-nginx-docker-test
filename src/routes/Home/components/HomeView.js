3b226728d999e2ecb3b3aad535c6bd0116932fef
SamDegDev <samdegdev@outlook.com>
2017-10-10 00:01:51 +0100

    first commit

---
+++ src/routes/Home/components/HomeView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DuckImage from '../assets/Duck.jpg'
+import classes from './HomeView.scss'
+
+export const HomeView = () => (
+  <div>
+    <h4>This is a React app test dockerized and deployed to AWS EB</h4>
+  </div>
+)
+
+export default HomeView
